08960f54cea58eeafde6e0e63c145c1afcbe148a
Wiwat <thegumzaa@gmail.com>
2017-09-26 17:57:31 +0700

    add app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from flask import jsonify, request
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1> Hello World!</h1>"
+
+
+app.run(host='0.0.0.0', port=8080)
