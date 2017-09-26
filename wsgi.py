4eb79d78109603025902088c59a76f55b255740b
Wiwat <thegumzaa@gmail.com>
2017-09-26 18:11:05 +0700

    test run on s2i

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    application.run()
